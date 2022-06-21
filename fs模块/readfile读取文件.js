15bff7b90ae98e57612e906f549e70f47b4a1cc4
zwp <1556249350@qq.com>
2022-06-21 17:03:03 +0800

    master

--- fs模块/readfile读取文件.js
+++ fs模块/readfile读取文件.js
@@ -1,8 +1,11 @@
 // 导入fs模块
 const fs = require('fs');
 
+console.log(fs)
+
 // 调用fs.readFile（）方法
-fs.readFile('./data.json',"utf-8", (err, data) => {
+// __dirname表示当前被运行的文件所处的目录
+fs.readFile(__dirname+'/data.json',"utf-8", (err, data) => {
     /**
      * 回调的两个参数
      *  参数一（err）：错误时的结果
