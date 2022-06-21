3e37adc964cbe005cc19667f6011bac4f00ea5c2
zwp <1556249350@qq.com>
2022-06-21 18:18:45 +0800

    03-path模块的extname方法修改了

--- path模块/03-path模块extname获取文件扩展名.js
+++ path模块/03-path模块extname获取文件扩展名.js
@@ -1,7 +1,12 @@
 import { default as path } from "path";
 
+// path.extname()返回值，如果有扩展名者返回对应扩展名，否则返回空字符串
+
 // .html
 console.log(path.extname("index.html"))
 
 // .html
 console.log(path.extname("./a/b/index.html"))
+
+// 空字符
+console.log(path.extname("./a/b/index"))
