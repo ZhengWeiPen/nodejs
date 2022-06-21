d8be93de83a28e624895f994cb34b99fe26b67ed
zwp <1556249350@qq.com>
2022-06-21 18:06:46 +0800

    修改了path模块

--- path模块/02-path模块的basename方法.js
+++ path模块/02-path模块的basename方法.js
@@ -3,9 +3,15 @@ import { default as path } from "path";
 let fileSrc = 'E:/web/案例/nodejs/day01/path模块/02-path模块的basename方法.js'
 
 /*
-    注意：第二个参数，如果没有选择该参数，者获取完整文件名（文件名+扩展名，当扩展名不匹配时也是如此
-    如果选择了该参数者只获取文件名
+    注意：第二个参数，为移除对应的文件扩展名，如果匹配者返回文件名，否则返回文件名+扩展名
 */
-let filename = path.basename(fileSrc, ".js")
+let filename = path.basename(fileSrc)
 // 02-path模块的basename方法.js
 console.log(filename);
+
+filename = path.basename(fileSrc, ".js")
+// 02-path模块的basename方法
+console.log(filename);
+
+// path模块
+console.log(path.basename("E:/web/案例/nodejs/day01/path模块/"))
